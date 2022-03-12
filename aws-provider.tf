d09c6baa8854e3b2b46b38580a55b6dda9331e0d
brycetrapier-morpheus <92887125+brycetrapier-morpheus@users.noreply.github.com>
2022-03-11 20:22:45 -0700

    Update aws-provider.tf

--- aws-provider.tf
+++ aws-provider.tf
@@ -2,9 +2,7 @@
 #
 provider "aws" {
    region = "us-west-1"
-  access_key = "AKIAW5SUXNJHFPVJ3O5B"
-  secret_key = "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"
   # region = var.region
-  # access_key = var.access_key
-  # secret_key = var.secret_key
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
